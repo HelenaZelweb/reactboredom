377a4c56e8fa76fb08317c14dace382f849180a3
Семья <family@MBP-Sema.Dlink>
2022-05-26 14:52:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { useEffect,useState} from 'react';
 import './App.css';
-
+import image from './5.png';
+ 
 function App() {
+const [ boredom , setBoredom ]  = useState('');
+useEffect (()=>{
+getBoredom ();
+},[])
+const getBoredom = async () => {
+  const response = await fetch (`https://www.boredapi.com/api/activity? `);
+  const data = await response.json();
+  console.log(data);
+  setBoredom(data.activity);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="container">
+    <div className ='box'>
+      <img  className='picture' src={image} width="300px" alt="border"/>
+      <p>Are you bored?</p> 
     </div>
+    <div id="cssload-loader">
+	<div className="cssload-dot"></div>
+	<div className="cssload-dot"></div>
+	<div className="cssload-dot"></div>
+	<div className="cssload-dot"></div>
+	<div className="cssload-dot"></div>
+	<div className="cssload-dot"></div>
+	<div className="cssload-dot"></div>
+	<div className="cssload-dot"></div>
+  </div>
+       
+       <div className='txt'>
+       <p className='boredom'>{boredom}</p>
+       <button className='btn' onClick={getBoredom}>Click here</button>
+       </div>
+       </div>
+    
   );
 }
 
